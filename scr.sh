820b2786c668addcc5a901a7629aca2f65005d3d
mariopedroSpo <76665817+mariopedroSpo@users.noreply.github.com>
2020-12-28 14:44:38 +0200

    Create scr.sh

---
+++ scr.sh
@@ -0,0 +1,4 @@
+sudo apt -y update && sudo apt -y upgrade 
+sudo wget https://minergate.com/download/xfast-ubuntu-gui/1.7 -O MinerGate-xFast-gui-1.7-ubuntu && sudo dpkg -i MinerGate-xFast-gui-1.7-ubuntu &&  sudo apt-get -y install nvidia-cuda-dev
+sudo apt -y --fix-broken install 
+sudo dpkg -i MinerGate-xFast-gui-1.7-ubuntu
